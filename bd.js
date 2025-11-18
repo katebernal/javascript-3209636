12887eda4d1ef60ae397947a5b884cd7ebc25aa7
katebernal <kcastrillon382@gmail.com>
2025-11-18 15:46:30 -0500

    Add files via upload

---
+++ bd.js
@@ -0,0 +1,95 @@
+let comic={
+  "nombrecomic": "adrian y las cronicas del imperio",
+  "sinopsis": "arquologo viajero del timepo",
+  "year": 2025,
+  "autores": ["stiven","hillary","kate","andres"],
+  "persoanjes": ["adrian","monje","socrates","platon","aristoteles","ninoc","senador"],
+  "genero": "ficiion",
+  "portada": "./imagenes/portada.jpg",
+    "escenas": [
+    {
+        "id":1,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    
+    {
+         "id":2,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      
+    },
+    {
+        "id":3,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+     {
+        "id":4,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+     {
+        "id":5,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+     {
+        "id":6,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+
+
+     {
+        "id":7,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+     {
+        "id":8,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+
+
+
+
+     {
+        "id":1,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+
+
+     {
+        "id":1,
+      "nombre": "Molecule Man",
+      "personajes": "Dan Jukes",
+      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+  ]
+}
