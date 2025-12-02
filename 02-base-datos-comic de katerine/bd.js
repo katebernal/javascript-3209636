365491d10e88fad62cbc92726d7f43f1a33633f0
katebernal <kcastrillon382@gmail.com>
2025-12-02 15:37:46 -0500

    Add files via upload

--- 02-base-datos-comic de katerine/bd.js
+++ 02-base-datos-comic de katerine/bd.js
@@ -1,55 +1,108 @@
-let comic={
+export let comic={
   "nombrecomic": "adrian y las cronicas del imperio",
   "sinopsis": "arquologo viajero del timepo",
   "year": 2025,
-  "autores": ["stiven","hillary","kate","andres"],
-  "persoanjes": ["adrian","monje","socrates","platon","aristoteles","ninoc","senador"],
+  "autores": ["Stiven","Hillary","Katherine","Andres"],
+  "personajes": [
+
+    { "id":1,
+      "nombre": "Adrian", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo "
+},
+ { "id":2,
+  "nombre": "Monje", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":3,
+  "nombre": "Ninov", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ { "id":4,
+  "nombre": "Platon", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ { "id":5,
+  "nombre": "Aristoteles", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":6,
+  "nombre": "Socrates", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":7,
+  "nombre": "Senador", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+}, ],
   "genero": "ficiion",
   "portada": "./imagenes/portada.jpg",
     "escenas": [
     {
         "id":1,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "nombre": "Monumento",
+      "personajes": "Adrian",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+       "ver": "mas capitulos",
     },
     
     {
          "id":2,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
-      
+      "nombre": " Paris",
+      "personajes": "monje",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
     {
         "id":3,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
-    },
+      "nombre": "Locas",
+      "personajes": "Platon",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+     "ver": "mas capitulos",  
+      },
 
 
      {
         "id":4,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "nombre": "Memedellín",
+      "personajes": "ninov",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
 
 
      {
         "id":5,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "nombre": "Bello",
+      "personajes": "aristoteles",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
 
 
      {
         "id":6,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "nombre": "Grecia",
+      "personajes": "socrates",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
 
 
@@ -57,17 +110,23 @@ let comic={
 
      {
         "id":7,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "nombre": "Roma",
+      "personajes": "senador",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
 
 
      {
         "id":8,
-      "nombre": "Molecule Man",
+      "nombre": "Coliseo",
       "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/movie.mp4",
+      "ver": "mas capitulos",
     },
 
 
@@ -75,21 +134,6 @@ let comic={
 
 
 
-     {
-        "id":1,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
-    },
-
-
-
-
-     {
-        "id":1,
-      "nombre": "Molecule Man",
-      "personajes": "Dan Jukes",
-      "image": ["Radiation resistance", "Turning tiny", "Radiation blast"]
-    },
+  
   ]
 }
