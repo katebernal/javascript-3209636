0ca0c58629f8b0f027b041b4a6f738ac2d014568
katebernal <kcastrillon382@gmail.com>
2025-12-10 14:19:59 -0500

    Add files via upload
    actualizado

---
+++ 02-base-datos-comic de katerine/bd.js
@@ -0,0 +1,139 @@
+export let comic={
+  "nombrecomic": "adrian y las cronicas del imperio",
+  "sinopsis": "arquologo viajero del timepo",
+  "year": 2025,
+  "autores": ["Stiven","Hillary","Katherine","Andres"],
+  "personajes": [
+
+    { "id":1,
+      "nombre": "Adrian", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo "
+},
+ { "id":2,
+  "nombre": "Monje", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":3,
+  "nombre": "Ninov", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ { "id":4,
+  "nombre": "Platon", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ { "id":5,
+  "nombre": "Aristoteles", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":6,
+  "nombre": "Socrates", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+},
+ {"id":7,
+  "nombre": "Senador", 
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+  "descripcion":"arqueologo "
+}, ],
+  "genero": "ficiion",
+  "portada": "./imagenes/portada.jpg",
+    "escenas": [
+    {
+        "id":1,
+      "nombre": "Monumento",
+      "personajes": "Adrian",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+       "ver": "mas capitulos",
+    },
+    
+    {
+         "id":2,
+      "nombre": " Poblado",
+      "personajes": "monje",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+    {
+        "id":3,
+      "nombre": "Manrique",
+      "personajes": "Platon",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+     "ver": "mas capitulos",  
+      },
+
+
+     {
+        "id":4,
+      "nombre": "Medellín",
+      "personajes": "Ninov",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+     {
+        "id":5,
+      "nombre": "Bello",
+      "personajes": "Aristoteles",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+     {
+        "id":6,
+      "nombre": "Grecia",
+      "personajes": "socrates",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"Arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+
+
+     {
+        "id":7,
+      "nombre": "Roma",
+      "personajes": "Senador",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+     {
+        "id":8,
+      "nombre": "Coliseo",
+      "personajes": "Dan Jukes",
+      "image": "./imagenes/imagenes-sin-fondo-hello-kitty.png",
+      "descripcion":"arqueologo ",
+      "video":"./imagenes/video.mp4",
+      "ver": "mas capitulos",
+    },
+
+
+
+
+
+
+  
+  ]
+}
