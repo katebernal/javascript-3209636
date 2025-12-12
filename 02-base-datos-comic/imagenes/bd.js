de9202e620b739576e3b8b63f563e3919d8c40c8
katebernal <kcastrillon382@gmail.com>
2025-12-11 22:03:39 -0500

    Add files via upload

---
+++ 02-base-datos-comic/imagenes/bd.js
@@ -0,0 +1,18 @@
+export let comic = {
+nombrecomic: "Adrián y las Crónicas del Imperio",
+sinopsis: "Un arqueólogo que viaja a través del tiempo descubriendo secretos antiguos.",
+
+
+escenas:[
+{id:1, nombre:"Monumento", image:"./imagenes/escena1.jpg", descripcion:"Adrián explora un monumento antiguo.", video:"./imagenes/video1.mp4"},
+{id:2, nombre:"Poblado", image:"./imagenes/escena2.jpg", descripcion:"Encuentro con un sabio del poblado.", video:"./imagenes/video2.mp4"},
+{id:3, nombre:"Coliseo", image:"./imagenes/escena3.jpg", descripcion:"Batalla en el coliseo.", video:"./imagenes/video3.mp4"}
+],
+
+
+personajes:[
+{id:1, nombre:"Adrián", image:"./imagenes/adrian.jpg", descripcion:"Protagonista principal."},
+{id:2, nombre:"Monje", image:"./imagenes/monje.jpg", descripcion:"Guardián ancestral."},
+{id:3, nombre:"Ninov", image:"./imagenes/ninov.jpg", descripcion:"Aliado en misiones."}
+]
+}
